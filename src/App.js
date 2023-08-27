413527ad4bcf269ce00eaf0643cc325355c49ca9
jarkko2 <77615804+jarkko2@users.noreply.github.com>
2023-08-27 10:34:44 +0300

    Added routing for login/shop/order views

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Login from './Login'
 import ParentComponent from './ParentComponent';
+import { Outlet, Link, useNavigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Login/>
-        <ParentComponent/>
-      </header>
+    <div className="App">   
+      <nav>
+          <div className="navBar">
+            <div className="navBarLeft">
+              <Link to="/login">Login</Link> | 
+              <Link to="/shopView"> ShopView</Link> | 
+              <Link to="/orderView"> OrderView</Link>
+            </div>
+          </div>
+        </nav>
+        <Outlet />
+      <header className="App-header"></header>
     </div>
   );
 }
---
+++ src/routes/orderView.js
@@ -0,0 +1,8 @@
+import OrderList from '../Orders'
+export default function OrderView() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <OrderList />
+    </main>
+  );
+}
