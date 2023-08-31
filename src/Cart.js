dd8741ef21ff6d84845fd9274d7231d0a8146b64
jarkko2 <niskanenjarkko1@gmail.com>
2023-08-31 13:21:25 +0300

    Started doing material UI for shopping cart

--- src/Cart.js
+++ src/Cart.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ShopItem from './ShopItem'
+import CartItem from './ItemComponents/CartItem'
 import { useGlobal } from './GlobalContext';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Container from '@mui/material/Container';
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import Grid from '@mui/material/Grid';
 
 function ShoppingList(itemAdded) {
   const { globalState, setGlobalState } = useGlobal();
@@ -15,14 +22,14 @@ function ShoppingList(itemAdded) {
   }, [globalState, isDeleted]);
 
   useEffect(() => {
-    if (isDeleted){
-        setDeleted(false)
+    if (isDeleted) {
+      setDeleted(false)
     }
   })
-  
+
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:5001/api/shoppingcart/names', {withCredentials: true})
+      const response = await axios.get('http://localhost:5001/api/shoppingcart/names', { withCredentials: true })
       console.log("shopping cart")
       console.log(response.data.items)
       setItems(response.data.items);
@@ -33,8 +40,8 @@ function ShoppingList(itemAdded) {
   };
 
   const itemClicked = async (id) => {
-    try {   
-      const response = await axios.put('http://localhost:5001/api/shoppingcart/'+ id, null, {withCredentials: true});
+    try {
+      const response = await axios.put('http://localhost:5001/api/shoppingcart/' + id, null, { withCredentials: true });
       console.log('Item removed from cart:', id);
       console.log(response)
       setDeleted(true)
@@ -46,16 +53,28 @@ function ShoppingList(itemAdded) {
   return (
     <div>
       <h1>Shopping cart</h1>
-      <ul className="shop-cart">
-        {items.map(item => (
-            <div>
-              <ShopItem item={item}/>
-              <button className="shop-button" onClick={() => itemClicked(item._id)}>Delete</button>
-            </div>
-          
-        ))} 
-      </ul>
+
+      <Container sx={{ py: 6 }} maxWidth="md">
+        <Grid item xs={12} md={6}>
+          {items.map(item => (
+            <List>
+              <ListItem>
+                <CartItem item={item} />
+                <IconButton edge="end" aria-label="delete" onClick={() => itemClicked(item._id)}>
+                  <DeleteIcon />
+                </IconButton>
+              </ListItem>
+            </List>
+          ))}
+        </Grid>
+      </Container>
+
       <p>Total: {total}</p>
+      <Grid item xs={12} md={6}>
+            <List dense={3}>
+
+            </List>
+        </Grid>
     </div>
   );
 }
--- src/ItemList.js
+++ src/ItemList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ShopItem from './ShopItem'
+import ShopItem from './ItemComponents/ShopItem'
 import { useGlobal } from './GlobalContext';
 
 
