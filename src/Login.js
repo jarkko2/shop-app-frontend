0532d692c4b921f7ae4af12a6bc7f57585c97e30
jarkko2 <niskanenjarkko1@gmail.com>
2023-09-04 12:57:34 +0300

    Added categorybar as seperate

--- src/Login.js
+++ src/Login.js
@@ -77,10 +77,6 @@ function Login() {
                 id="password"
                 autofocus
               />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
               <Button
                 type="submit"
                 fullWidth
@@ -90,11 +86,6 @@ function Login() {
                 Sign In
               </Button>
               <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
                 <Grid item>
                   <Link href="register" variant="body2">
                     {"Don't have an account? Sign Up"}
